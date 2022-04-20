2599610c7c1b6778bd00453a0cd4ebcba513b690
Alex Mononen <alex@mononen.dev>
2022-04-20 13:58:21 -0400

    switched queue implementation and overclocked to 160mhz
    serial out is still not providing anywhere near close enough info, but I suspect that's the speed of the serial monitor

--- include/sense.h
+++ include/sense.h
@@ -1,9 +1,15 @@
 #ifndef SENSE_H
 #define SENSE_H
 
+struct Pulse {
+  unsigned long rising;
+  unsigned long falling;
+  int time;
+};
+
 void sense_init();
 void sense_loop();
 
-void IRAM_ATTR risingInterrupt();
+void IRAM_ATTR pinInterrupt();
 
 #endif // !SENSE_H
--- src/sense.cpp
+++ src/sense.cpp
@@ -1,22 +1,28 @@
 #include <Arduino.h>
-#include <pulse.h>
-#include <queue>
 
 #include "sense.h"
 
 #define inPin 5 //gpio pin 5, marked as D1 on the board
 
-std::vector<Pulse> pulseList; 
+#define pulseBufferSize 32
 
-void risingInterrupt() {
-  pulseList.insert(pulseList.begin(), Pulse(micros()));
+Pulse pulseBuffer[pulseBufferSize]; //buffer for pulse data
+int pulseBufferIndex = 0; //index of the next pulse to be added to the buffer
+
+void pinInterrupt() {
+  if (digitalRead(inPin) == HIGH) { //investigate direct port read
+    pulseBuffer[pulseBufferIndex].rising = micros();
+    pulseBufferIndex++;
+    if (pulseBufferIndex >= pulseBufferSize) {
+      pulseBufferIndex = 0;
+    }
+  } else {
+    pulseBuffer[pulseBufferIndex].falling = micros();
+  }
 }
 
 void sense_init() {
   delay(3000);
-
-  //init the queue of pulses
-  pulseList = std::vector<Pulse>();
 
   Serial.begin(115200);
   pinMode(inPin, INPUT);
@@ -24,21 +30,23 @@ void sense_init() {
   // pinMode(LED_BUILTIN, OUTPUT);
 
   //hooks interrupt to pin
-  attachInterrupt(digitalPinToInterrupt(inPin), risingInterrupt, RISING);
-
+  attachInterrupt(digitalPinToInterrupt(inPin), pinInterrupt, CHANGE);
 }
+
+Pulse p;
+int idx;
 
 void sense_loop() {
 
-  while(!pulseList.empty()) {
-    Pulse p = pulseList[0]; //gets the first pulse in the queue data
-    // int t = p.computeTime(); //computes the time of the pulse
-    pulseList.erase(pulseList.begin()); //erases the pulse from the list
-    // digitalWrite(LED_BUILTIN, HIGH);
-    // Serial.println("pulse");
-    Serial.println("----Pulse found with rising: " + String(p.getRising()));
+  while(pulseBuffer[pulseBufferIndex].rising != 0) {
+    idx = pulseBufferIndex;
+    p = pulseBuffer[idx]; //gets the first pulse in the queue data
+    // Serial.println("----Pulse found with rising: " + String(p.rising) + " falling: " + String(p.falling));
+    Serial.println("idx is: " + String(idx) + " rising: " + String(p.rising) + " falling: " + String(p.falling));
+
+    pulseBuffer[idx].rising = 0; //'erases' the pulse from the list
     // p.log(); //prints the pulse to the serial monitor
   }
-  Serial.println("N");
+  // Serial.println("N");
   // digitalWrite(LED_BUILTIN, LOW);
 }
